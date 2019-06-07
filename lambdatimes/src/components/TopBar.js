0894eae594abfa03cedb572f7f37ef72c5fc4961
hsrsbd <hsraffety@gmail.com>
2019-06-07 09:57:55 -0700

    working on styled components and prop-checks

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,24 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
 
-// Refactor this component to use styled components and not classNames. 
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
+
+const TopBarContainer = styled.div``;
+
+const Container = styled.div``;
+
+const ContainerLeft = styled.div``;
+
+const Spans = styled.span``;
+
+const ContainerCenter = styled.div``;
+
+const ContainerRight = styled.div``;
 
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
+    <div className='top-bar'>
+      <div className='container'>
+        <div className='container-left'>
+          <span>TOPICS</span>
+          <span>SEARCH</span>
         </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
+        <div className='container-center'>
+          <span>GENERAL</span>
+          <span>BROWNBAG</span>
+          <span>RANDOM</span>
+          <span>MUSIC</span>
+          <span>ANNOUNCEMENTS</span>
         </div>
-        <div className="container-right">
+        <div className='container-right'>
           <span>LOG IN</span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default TopBar;
